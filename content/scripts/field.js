caf97ce8806f07e78e59bc29d44c71c3c58d9c51
matthewsotoudeh <matthewsot@outlook.com>
2014-09-16 21:15:56 -0700

    start of the js to find the set on a grid
    treats the field as a grid of steps

---
+++ content/scripts/field.js
@@ -0,0 +1,22 @@
+//x in the form
+//side (side): (steps) (in/out) from (yardline)
+function setFromXY(x, y) {
+    var side = parseFloat(x.split("side")[1].split(":")[0].trim());
+    var steps = parseFloat(x.split(":")[1].trim().split(" ")[0].trim());
+    var direction = x.split("from")[0].split(":")[1].trim().split(" ")[1].trim();
+    var reference = parseFloat(x.split("from")[1].trim());
+    
+    //get the reference as an X value in steps
+    var referenceX = ((50 - reference) * 8) * (side == 1 ? -1 : 1);
+    var actualX;
+    
+    //get the difference
+    if(side == 1) {
+        if(direction == "in") {
+            actualX = referenceX + steps;
+        }
+        else if (direction == "out") {
+            actualX = referenceX - steps;
+        }
+    }
+}
